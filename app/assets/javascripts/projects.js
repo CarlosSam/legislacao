356775f8c67f5f5698c4fd4b855112b3f6eb80af
Alisson Nunes <alisson@alisson-SVD11215CDB>
2016-09-03 23:23:54 -0300

    tela de cadastro de projetos

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,15 @@
+$("#0 .btn").click(function(event){
+	$("#1").removeClass("hidden");
+});
+
+$("#1 .btn").click(function(event){
+	$("#2").removeClass("hidden");
+});
+
+$("#2 .btn").click(function(event){
+	$("#3").removeClass("hidden");
+});
+
+$("#3 .btn").click(function(event){
+	$("#4").removeClass("hidden");
+});
